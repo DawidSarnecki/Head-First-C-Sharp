61cd5e232285098877075dba1f474f8b4078a4f5
DawidSarnecki <sarnecki.dawid@wp.pl>
2017-06-04 21:26:26 +0200

    Examples of using delegates

---
+++ SimpleExmples/AnonymousFunctions/AnonymousFunctions/Example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousFunctions
+{
+    class Example
+    {
+        public Example() {
+
+            this.FistWayOfDelegateSyntax("FistWayOfDelegateSyntax: I'm delegate initialized with a named method.");
+            this.SecondWayOfDelegateSytntax("SecondWayOfDelegateSytntax: I'm delegate initialized with inline code, called an anonymous method");
+            this.ThirdwAyOfDElegateSyntax("ThirdwAyOfDElegateSyntax: I'm delegate initialized with a lambda expression.");
+            this.exampleWithoutParams();
+        }
+
+
+        public delegate void ExampleDelegate(string textValue);
+
+        static void ShowText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
+        // 1. way:
+        //Original syntax required initialization with a named method.
+        ExampleDelegate FistWayOfDelegateSyntax = new ExampleDelegate(ShowText);
+
+        //2. way
+        // C# 2.0: initialization with inline code, called an "anonymous method." This
+        // method takes a specific as an input parameter (in our example is string).
+        ExampleDelegate SecondWayOfDelegateSytntax = delegate (string anyText) { Console.WriteLine(anyText); };
+
+
+        //3. way
+        // C# 3.0. A delegate can be initialized with a lambda expression. 
+        //The lambda also takes a string
+        // as an input parameter (x). The type of x is inferred by the compiler.
+        ExampleDelegate ThirdwAyOfDElegateSyntax = (x) => { Console.WriteLine(x); };
+
+        delegate void ExampleDelegateWithoutArguments();
+        ExampleDelegateWithoutArguments exampleWithoutParams = () => { Console.WriteLine("I'm a delegate initialized using lambda expresions but without params."); };
+
+
+    }
+}
---
+++ SimpleExmples/AnonymousFunctions/AnonymousFunctions/Program.cs
@@ -0,0 +1,27 @@
+﻿///source: https://docs.microsoft.com/pl-pl/dotnet/csharp/programming-guide/statements-expressions-operators/anonymous-functions
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnonymousFunctions;
+
+namespace AnonymousFunctions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Example example = new Example();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
