ef6bf8350b4f2e77a4d01bd0561edf1d98b5e7ab
DawidSarnecki <sarnecki.dawid@wp.pl>
2017-06-12 22:27:31 +0200

    Added new class

---
+++ SimpleExmples/DictionaryVsOrderedDictionary/DictionaryVsOrderedDictionary/Implementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryVsOrderedDictionary
+{
+    class Implementation
+    {
+        private Dictionary<int, String> SomeDictionary {get; set;}
+
+        private OrderedDictionary SomeOrderedDctionary {get; set;}
+
+        public Implementation()
+        {
+            this.SomeDictionary = new Dictionary<int, string>();
+            this.SomeOrderedDctionary = new OrderedDictionary();
+
+           
+            for (int i = 0; i < 100; i++)
+            {
+                SomeDictionary.Add(i, (i*2).ToString());
+                SomeOrderedDctionary.Add(i, (i*2).ToString());
+            }
+
+            Console.WriteLine("SomeDictionary");
+            foreach (var item in SomeDictionary)
+            {
+                Console.WriteLine($"key: {item.Key}; value: {item.Value}");
+            }
+
+
+            Console.WriteLine("SomeOrderedDctionary");
+            foreach (DictionaryEntry item in SomeOrderedDctionary)
+            {
+                Console.WriteLine($"key: {item.Key}; value: {item.Value}");
+            }
+
+        }
+
+    }
+}
